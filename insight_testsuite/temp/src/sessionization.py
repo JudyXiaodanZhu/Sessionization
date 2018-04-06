937596e2d8681371e51b1379d5d67565e9206b52
Judy Zhu <jz_s39@hotmail.com>
2018-04-06 09:14:17 -0400

    1st commit

---
+++ insight_testsuite/temp/src/sessionization.py
@@ -0,0 +1,62 @@
+import csv
+import datetime
+import sys
+
+csvFile = sys.argv[1]
+textFile = sys.argv[2]
+outFile = sys.argv[3]
+
+map = dict()
+file = open(textFile, "r")
+check_time = int(file.read())
+file.close()
+
+
+def process_data():
+    count = 0
+    with open(csvFile, 'rb') as csvfile:
+        session_reader = csv.reader(csvfile)
+        next(session_reader, None)
+        for line in session_reader:
+            user = line[0]
+            dt_obj = datetime.datetime.strptime(line[1]+" "+line[2], "%Y-%m-%d %H:%M:%S")
+
+            for person in map.keys():
+                if dt_obj > map[person]['time_count']:
+                    write_to_file(person, map[person])
+                    map.pop(person)
+
+            if user not in map:
+                map[user] = dict()
+                map[user]['first_time'] = dt_obj
+                map[user]['last_time'] = dt_obj
+                map[user]['time_count'] = datetime.timedelta(seconds=check_time) + dt_obj
+                map[user]['web_count'] = 1
+                map[user]['count'] = count
+                count += 1
+            else:
+                map[user]['last_time'] = dt_obj
+                map[user]['web_count'] += 1
+                map[user]['time_count'] = datetime.timedelta(seconds=check_time) + dt_obj
+
+    while map:
+        user = min(map.keys(), key=lambda x: map[x]['count'])
+        write_to_file(user, map[user])
+        map.pop(user)
+
+
+def write_to_file(id, line):
+    session = int((map[id]['last_time'] - map[id]['first_time']
+                                                 + datetime.timedelta(seconds=1)).total_seconds())
+    first = line['first_time'].strftime("%Y-%m-%d %H:%M:%S")
+    last = line['last_time'].strftime("%Y-%m-%d %H:%M:%S")
+    string = id + ','+first+','+last+','+str(session)+','+str(line['web_count'])
+
+    file = open(outFile, "a")
+    file.write(string+'\n')
+    file.close()
+    return
+
+
+process_data()
+
